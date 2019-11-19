6bb599b2674e32688a1313528ea7362a80dcc915
Daniel Batista <2171836@my.ipleiria.pt>
2019-11-19 09:44:37 +0000

    Adding Activities

---
+++ app/src/main/java/amsi/estg/alumio/AlunoActivity.java
@@ -0,0 +1,14 @@
+package amsi.estg.alumio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno);
+    }
+}
