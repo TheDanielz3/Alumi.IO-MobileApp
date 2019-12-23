6d401b788bca2d99836bcad25644593745308130
Daniel Batista <2171836@my.ipleiria.pt>
2019-12-23 17:29:54 +0000

    Creating a JsonParcer

---
+++ app/src/main/java/com/example/alumiio/utils/AlunoJsonParser.java
@@ -0,0 +1,40 @@
+package com.example.alumiio.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.alumiio.models.Aluno;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class AlunoJsonParser {
+
+    public static ArrayList<Aluno> parserJsonAlunos(JSONArray response, Context context){
+
+        ArrayList<Aluno> tempAlunoList = new ArrayList<>();
+
+        try {
+
+            for (int i = 0; i < response.length(); i++){
+                JSONObject aluno = (JSONObject) response.get(i);
+                int alunoID = aluno.getInt("id");
+                String nome = aluno.getString("nome");
+
+
+
+
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Error: " + e.getMessage(),Toast.LENGTH_LONG).show();
+
+        }
+        return tempAlunoList;
+    }
+
+}
